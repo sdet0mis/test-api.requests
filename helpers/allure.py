b37a5279a6ab9ea0068d127dffdd7414c88e3e39
sdet0mis <sdet0mis@mail.ru>
2025-05-05 13:04:42 +0300

    add helpers

---
+++ helpers/allure.py
@@ -0,0 +1,14 @@
+import allure
+from allure_commons.types import AttachmentType
+from requests import Response
+
+
+class Allure:
+
+    @staticmethod
+    def attach_response_body(response: Response) -> None:
+        allure.attach(
+            response.content,
+            "Тело ответа",
+            AttachmentType.JSON
+        )
---
+++ helpers/db.py
@@ -0,0 +1,64 @@
+import json
+import os
+
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class DBConnector():
+
+    def __init__(self) -> None:
+        self.connection = mysql.connector.connect(
+            **json.loads(os.getenv("DB"))
+        )
+        assert self.connection.is_connected(), "Соединение с БД не установлено"
+
+    def get_user_by_id(self, uid: int) -> list:
+        self.cursor = self.connection.cursor()
+        self.cursor.execute(
+            """SELECT user_login, user_email FROM wp_users WHERE id = %s""",
+            [uid]
+        )
+        user = self.cursor.fetchall()
+        self.connection.commit()
+        self.cursor.close()
+        return user
+
+    def get_page_by_id(self, pid: int) -> list:
+        self.cursor = self.connection.cursor()
+        self.cursor.execute(
+            """SELECT post_title FROM wp_posts WHERE id = %s""",
+            [pid]
+        )
+        page = self.cursor.fetchall()
+        self.connection.commit()
+        self.cursor.close()
+        return page
+
+    def get_post_by_id(self, pid: int) -> list:
+        self.cursor = self.connection.cursor()
+        self.cursor.execute(
+            """SELECT post_title FROM wp_posts WHERE id = %s""",
+            [pid]
+        )
+        post = self.cursor.fetchall()
+        self.connection.commit()
+        self.cursor.close()
+        return post
+
+    def get_comment_by_id(self, cid: int) -> list:
+        self.cursor = self.connection.cursor()
+        self.cursor.execute(
+            """SELECT comment_content, comment_post_ID FROM wp_comments \
+                WHERE comment_ID = %s""",
+            [cid]
+        )
+        comment = self.cursor.fetchall()
+        self.connection.commit()
+        self.cursor.close()
+        return comment
+
+    def disconnect(self) -> None:
+        self.connection.close()
