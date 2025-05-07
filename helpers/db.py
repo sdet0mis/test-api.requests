feec8ee837e87978ae2ee9191387253089e4657e
sdet0mis <sdet0mis@mail.ru>
2025-05-07 11:35:55 +0300

    add request method in db connector

--- helpers/db.py
+++ helpers/db.py
@@ -14,50 +14,39 @@ class DBConnector():
         )
         assert self.connection.is_connected(), "Соединение с БД не установлено"
 
-    def get_user_by_id(self, uid: int) -> list:
+    def db_request(self, query: tuple[str, list]) -> list:
         self.cursor = self.connection.cursor()
-        self.cursor.execute(
-            """SELECT user_login, user_email FROM wp_users WHERE id = %s""",
-            [uid]
-        )
-        user = self.cursor.fetchall()
+        self.cursor.execute(*query)
+        data = self.cursor.fetchall()
         self.connection.commit()
         self.cursor.close()
-        return user
+        return data
+
+    def get_user_by_id(self, uid: int) -> list:
+        return self.db_request(
+            (
+                """SELECT user_login, user_email FROM wp_users \
+                        WHERE id = %s""", [uid]
+            )
+        )
 
     def get_page_by_id(self, pid: int) -> list:
-        self.cursor = self.connection.cursor()
-        self.cursor.execute(
-            """SELECT post_title FROM wp_posts WHERE id = %s""",
-            [pid]
+        return self.db_request(
+            ("""SELECT post_title FROM wp_posts WHERE id = %s""", [pid])
         )
-        page = self.cursor.fetchall()
-        self.connection.commit()
-        self.cursor.close()
-        return page
 
     def get_post_by_id(self, pid: int) -> list:
-        self.cursor = self.connection.cursor()
-        self.cursor.execute(
-            """SELECT post_title FROM wp_posts WHERE id = %s""",
-            [pid]
+        return self.db_request(
+            ("""SELECT post_title FROM wp_posts WHERE id = %s""", [pid])
         )
-        post = self.cursor.fetchall()
-        self.connection.commit()
-        self.cursor.close()
-        return post
 
     def get_comment_by_id(self, cid: int) -> list:
-        self.cursor = self.connection.cursor()
-        self.cursor.execute(
-            """SELECT comment_content, comment_post_ID FROM wp_comments \
-                WHERE comment_ID = %s""",
-            [cid]
+        return self.db_request(
+            (
+                """SELECT comment_content, comment_post_ID FROM wp_comments \
+                    WHERE comment_ID = %s""", [cid]
+            )
         )
-        comment = self.cursor.fetchall()
-        self.connection.commit()
-        self.cursor.close()
-        return comment
 
     def disconnect(self) -> None:
         self.connection.close()
